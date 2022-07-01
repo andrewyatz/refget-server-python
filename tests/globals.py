e87f9c7fa3a1f2d952155af5f1f0138cb3e48d46
Andrew Yates <ayates@ebi.ac.uk>
2022-07-01 13:09:27 +0100

    Apache 2.0 license

--- tests/globals.py
+++ tests/globals.py
@@ -1,3 +1,18 @@
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 sha512t24u = "aKF498dAxcJAqme6QYQ7EZ07-fiw8Kw2"
 ga4gh = f"ga4gh:SQ.{sha512t24u}"
 trunc512 = "68a178f7c740c5c240aa67ba41843b119d3bf9f8b0f0ac36"
